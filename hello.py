49cf7f5dca37f6c9f3dc0788562243988e967f33
JimmyGenius <jimiwang@utexas.edu>
2020-09-22 23:12:35 -0500

    update in pycharm

--- hello.py
+++ hello.py
@@ -2,4 +2,7 @@ print('hello world')
 print('this is my first try of git')
 print('I first clone the project to my file')
 print('then I git add, git status, git commit -m, git push')
-print('finanlly, I have the access') 
+print('finanlly, I have the access')
+
+print('now i am using the github to process the code')
+print('i would like to know the process and the difference')
